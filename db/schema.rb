41971fd156189901d3589a79addd8b48de17a76a
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-10-31 17:15:34 +0800

    Migrate from SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2023_10_30_150720) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "questions", force: :cascade do |t|
     t.string "question"
     t.text "answer"
