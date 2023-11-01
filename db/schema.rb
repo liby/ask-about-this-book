df6a9b0c986fd5c2cf5bbafd16174df5b1525ba6
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-11-02 01:05:37 +0800

    Deploy application to Railway

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2023_10_30_150720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "questions", force: :cascade do |t|
     t.string "question"
